0311f88f3a7afdeec7958f1a97a787b9544f07dc
Lemons <24715897+Lemons1337@users.noreply.github.com>
2019-05-19 10:42:24 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,174 @@
+// ==UserScript==
+// @name         Blob.io Bots
+// @namespace    https://youtube.com/Lemons1337
+// @version      1
+// @description  Blob.io bots!
+// @author       Lemons
+// @match        *://client.blobgame.io/*
+// @run-at       document-start
+// @grant        none
+// ==/UserScript==
+
+window.addEventListener("load", () => {
+    html.onScriptDownloaded(`
+        class User {
+
+            constructor() {
+                this.wsIp = 'ws://localhost:8080';
+                this.started = false;
+                this.x = this.y = 0;
+                this.mouseInt = -1;
+                this.byteLen = 0;
+                this.server = '';
+                this.mouse = 0;
+                this.ws = null;
+                this.connect();
+                this.addUI();
+            }
+
+            connect() {
+                this.ws = new WebSocket(this.wsIp);
+                this.ws.onmessage = this.onmessage.bind(this);
+                this.ws.onerror = this.onerror.bind(this);
+                this.ws.onclose = this.onclose.bind(this);
+                this.ws.onopen = this.onopen.bind(this);
+            }
+
+            onopen() {
+                this.startMouseInt();
+                this.started = false;
+            }
+
+            startMouseInt() {
+
+                this.mouseInt = setInterval(() => {
+
+                    let json = {};
+
+                    json.type = "mouse";
+                    json.packet = this.mouse;
+                    //json.x = this.x;
+                    //json.y = this.y;
+
+                    this.send(json);
+
+                }, 100);
+
+            }
+
+            onmessage(message) {
+                document.getElementById('botCount').innerText = message.data;
+            }
+
+            onclose() {
+                setTimeout(this.connect(), 1500);
+                clearInterval(this.mouseInt);
+            }
+
+            startBots() {
+                let json = {};
+
+                json.type = "start";
+                json.ip = client.server;
+
+                this.send(json);
+            }
+
+            stopBots() {
+                this.send({
+                    type: 'stop'
+                });
+            }
+
+            addUI() {
+                let ui = document.createElement('div');
+                ui.id = 'botcanvas';
+                ui.style['background'] = 'rgba(0,0,0,0.4)';
+                ui.style['top'] = '250px';
+                ui.style['left'] = '17px';
+                ui.style['display'] = 'block';
+                ui.style['position'] = 'absolute';
+                ui.style['text-align'] = 'center';
+                ui.style['font-size'] = '15px';
+                ui.style['color'] = '#FFFFFF';
+                ui.style['padding'] = '7px';
+                ui.style['z-index'] = '1000000';
+                ui.innerHTML += 'Bots: ';
+                let count = document.createElement('span');
+                count.id = 'botCount';
+                count.innerHTML = 'WAITING';
+                ui.appendChild(count);
+                document.body.appendChild(ui);
+                document.getElementById('botcanvas').onclick = () => {
+                    if (!this.started)
+                        this.startBots();
+                    else
+                        this.stopBots();
+                    this.started = !this.started;
+                };
+            }
+
+            onerror() { }
+
+            send(message) {
+                if (this.ws && this.ws.readyState == 1) this.ws.send(JSON.stringify(message));
+            }
+
+            get isTyping() {
+                return document.querySelectorAll('input:focus').length;
+            }
+
+            keyDown(event) {
+                if (this.isTyping || !event.key) return;
+                switch (event.key.toLowerCase()) {
+                    case 'e':
+                        this.send({
+                            type: 'split'
+                        });
+                        break;
+
+                    case 'r':
+                        this.send({
+                            type: 'eject'
+                        });
+                        break;
+
+                }
+            }
+
+        }
+            window.client = new User();
+            window.started = false;
+            document.addEventListener('keydown', window.client.keyDown.bind(window.client));
+            (function () {
+                WebSocket.prototype._send = WebSocket.prototype.send;
+                WebSocket.prototype.send = function (data) {
+                    this._send(data);
+                    var buf = new Uint8Array(data.buffer ? data.buffer : data);
+                    switch (buf[0]) {
+                        case 16:
+                            window.client.mouse = [...buf];
+                            break;
+
+                        case 255:
+                            if (!window.started) client.startBots();
+                            window.started = true;
+                            break;
+
+                        case 22:
+                            client.send({
+                                type: 'split'
+                            });
+                            break;
+
+                        case 23:
+                            this.send({
+                                type: 'eject'
+                            });
+                            break;
+                    }
+                    window.client.server = this.url;
+                };
+            })();
+    `);
+});
---
+++ server.js
@@ -0,0 +1,175 @@
+const proxyAgent = require('proxy-agent');
+const WebSocket = require('ws');
+const fs = require('fs');
+
+const proxies = fs.readFileSync('./proxies.txt', 'utf-8').replace(/\r/g, '').split('\n');
+
+const nick = 'Agar API';
+const botAmount = 300;
+
+class Bot {
+    constructor(server, id) {
+        this.id = id + 1;
+        this.server = server;
+        this.stopped = false;
+        this.connect();
+    }
+    connect() {
+        if (this.stopped) return;
+        this.proxy = proxies[~~(Math.random() * proxies.length)];
+        this.ws = new WebSocket(this.server, ['1', '3', 'WaWdft'].join(', '), {
+            agent: new proxyAgent('http://' + this.proxy),
+            headers: {
+                'Origin': 'https://client.blobgame.io',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'
+            }
+        });
+        this.ws.binaryType = 'nodebuffer';
+        this.ws.onmessage = this.onmessage.bind(this);
+        this.ws.onopen = this.onopen.bind(this);
+        this.ws.onclose = this.onclose.bind(this);
+        this.ws.onerror = () => { };
+        setInterval(() => {
+            // this.sendChat('Agar API');
+            this.spawn();
+        }, 2500);
+    }
+    spawn() {
+        var skins = ['fly', 'fish', 'amber', 'spider', 'small_chick', 'carp', 'lobster', 'wasp', 'gopher', 'chick', 'sea_turtle', 'octopus', 'lizard', 'rabbit', 'pug', 'mouse', 'birdie', 'bat', 'owl', 'squirrel', 'rooster', 'cat', 'snake', 'crow', 'parrot', 'prey', 'chihuahua', 'fox', 'desert_fox', 'pig', 'dog', 'blackcat', 'coyote', 'goat', 'deer', 'bullking', 'seal', 'fury_cat', 'penguin', 'blueswirl', 'sly', 'husky', 'sheep', 'panda', 'cute_panda', 'angry_panda', 'bear', 'bear_', 'bearr', 'rhino_boxer', 'cougar', 'wolf', 'wolff', 'spirxo', 'sabertooth', 'panther', 'kempo_tiger', 'dark_wings', 'firebird', 'wolf_', 'lion_', 'yeti', 'lion', 'leo', 'king_lion', 'crocodile', 'croc', 'jackal', 'taurus', 'shark', 'colossus', 'orc_grunt', 'behemoth', 'mammoth', 'silver_tusk', 'dragon', 'beast', 'raptor', 't_rex', 'godzilla', 'basilisk', 'sentinel', 'poseidon', 'kraken', 'red_fiend', 'wendigo', 'jotun', 'ice_lord', 'medusa', 'reaper'];
+        var skin = skins[~~(Math.random() * skins.length)];
+        var name = `<${skin}>${nick}`;
+        var buf = Buffer.alloc(1 + Buffer.byteLength(name, 'utf16le'));
+        buf.write(name, 1, 'utf16le');
+        this.send(buf);
+    }
+    sendChat(message) {
+        var buf = Buffer.alloc(2 + Buffer.byteLength(message, 'utf16le'));
+        buf.writeUInt8(99, 0);
+        buf.writeUInt8(64, 1);
+        buf.write(message, 2, 'utf16le');
+        this.send(buf);
+    }
+    onclose(err) {
+        setTimeout(this.connect.bind(this), 2500);
+    }
+    onopen() {
+        var buf = Buffer.alloc(5);
+        buf.writeUInt8(254, 0);
+        buf.writeUInt16LE(5, 1);
+        this.send(buf);
+
+        buf = Buffer.alloc(5);
+        buf.writeUInt8(255, 0);
+        buf.writeUInt32LE(154669603, 1);
+        this.send(buf);
+    }
+    stop() {
+        this.stopped = true;
+        this.ws.onclose();
+    }
+    send(buf) {
+        if (this.stopped) return;
+        if (this.ws && this.ws.readyState === WebSocket.OPEN) {
+            this.ws.send(buf);
+        }
+    }
+    split() {
+        var buf = Buffer.alloc(1);
+        buf.writeUInt8(17, 0);
+        this.send(buf);
+    }
+    eject() {
+        var buf = Buffer.alloc(1);
+        buf.writeUInt8(21, 0);
+        this.send(buf);
+    }
+    onmessage(message) {
+        var buf = Buffer.from(message.data);
+        var offset = 0;
+        switch (buf.readUInt8(offset++)) {
+            default:
+                break;
+        }
+    }
+}
+
+class Client {
+    constructor(ws) {
+        this.ws = ws;
+        this.bots = [];
+        this.ws.on('message', m => this.onmessage(m));
+        this.ws.on('close', e => this.onclose(e));
+        this.botCount();
+    }
+    onclose() {
+        clearInterval(this.int);
+    }
+    send(data) {
+        if (this.ws.readyState === WebSocket.OPEN) {
+            this.ws.send(data);
+        }
+    }
+    botCount() {
+        this.int = setInterval(() => {
+            var count = this.bots.filter(b => b.ws && b.ws.readyState === WebSocket.OPEN).length;
+            this.send(count + '/' + botAmount);
+        }, 250);
+    }
+    onmessage(message) {
+        var json = JSON.parse(message);
+        try {
+            switch (json.type) {
+                case 'mouse':
+                    if (typeof json.packet !== 'object') break;
+                    var buf = Buffer.from(json.packet);
+                    this.bots.forEach(bot => bot.send(buf));
+                    break;
+
+                case 'start':
+                    this.bots = [];
+                    for (var i = 0; i < botAmount; i++) {
+                        var bot = new Bot(json.ip, i);
+                        this.bots.push(bot);
+                    }
+                    break;
+
+                case 'stop':
+                    this.bots.forEach(bot => bot.stop());
+                    break;
+
+                case 'split':
+                    this.bots.forEach(bot => bot.split());
+                    break;
+
+                case 'eject':
+                    this.bots.forEach(bot => bot.eject());
+                    break;
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    }
+}
+
+var useSSL = false;
+
+if (useSSL) {
+    var https = require('https');
+
+    var app = https.createServer({
+        key: fs.readFileSync('/root/key.pem'),
+        cert: fs.readFileSync('/root/cert.pem')
+    }).listen(8443);
+
+    const wss = new WebSocket.Server({ server: app });
+
+    wss.on('connection', (ws, req) => {
+        ws.Client = new Client(ws, req);
+    });
+} else {
+    const wss = new WebSocket.Server({ port: 8080 });
+
+    wss.on('connection', (ws, req) => {
+        ws.Client = new Client(ws, req);
+    });
+}
